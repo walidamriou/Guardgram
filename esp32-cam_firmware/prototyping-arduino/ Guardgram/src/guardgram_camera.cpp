02801aafaa7da02e504a594a507cdb0faba9b462
Walid Amriou <walidamriou@gmail.com>
2021-07-04 15:48:22 +0100

    added guardgram_camera

---
+++ esp32-cam_firmware/prototyping-arduino/ Guardgram/src/guardgram_camera.cpp
@@ -0,0 +1,49 @@
+#include "guardgram_camera.h"
+
+// Initializes the ESP32 camera
+void Guardgram_Camera_Config(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // Init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  // Drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_CIF);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+}
---
+++ esp32-cam_firmware/prototyping-arduino/ Guardgram/src/guardgram_camera.h
@@ -0,0 +1,22 @@
+/**
+ * @file guardgram_camera.h
+ * @brief Guardgram camera by Arduino Lang header file
+ *
+ *  
+ * @author Walid Amriou
+ * @date 07/04/2021
+ * 
+ * 
+ */
+
+#ifndef __GUARDGRAM_CAMERA_H__
+#define __GUARDGRAM_CAMERA_H__
+
+#include "guardgram_includes.h"
+
+/**
+ * @brief Config the camera that used with Guardgram 
+ */
+void Guardgram_Camera_Config();
+
+#endif
