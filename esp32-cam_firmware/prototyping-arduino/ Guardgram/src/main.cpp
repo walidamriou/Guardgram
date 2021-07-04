0d8a2f813e1fbfd248a6ac63cdc75fd54690c723
Walid Amriou <walidamriou@gmail.com>
2021-07-04 15:49:23 +0100

    Update main.cpp

--- esp32-cam_firmware/prototyping-arduino/ Guardgram/src/main.cpp
+++ esp32-cam_firmware/prototyping-arduino/ Guardgram/src/main.cpp
@@ -1,9 +1,32 @@
-#include <Arduino.h>
+/**
+ * @file main.cpp
+ * @brief Main file of Guardgram firmware source by Arduino 
+ * 
+ * @author Walid Amriou
+ * @date 07/04/2021
+ * 
+ * Guardgram 
+ * Controlling ESP32-CAM via Telegram bot ( take and send photos ) 
+ * Source: https://github.com/walidamriou/Guardgram
+ * 
+ */
 
-void setup() {
-  // put your setup code here, to run once:
+#include "guardgram_includes.h"
+
+
+void setup(){
+  // General initializations
+  Guardgram_Init();
+
+  // Config and init the camera
+  Guardgram_Camera_Config();
+  
+  // Config WiFi connection
+  Guardgram_Wifi_Connect();
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  Guardgram_Core();
 }
+
